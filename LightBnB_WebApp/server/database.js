c78db39aea9e27d9228444dc3675f944b2f7a37e
Jeffrey <jeffrey-lee@live.ca>
2022-08-25 22:53:44 +0000

    LighthouseBnB Properties

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -124,17 +124,80 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
- const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      console.log(result.rows);
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+ const getAllProperties = function (options, limit = 10) {
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  
+
+    
+  
+  
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(`%${options.owner_id}%`);
+    if(queryParams.length > 0){
+      queryString += `AND owner_id = $${queryParams.length} `;
+    }else {
+      queryString += `WHERE owner_id = $${queryParams.length} `;
+    }
+    
+  }
+
+  if (options.minimum_price_per_night) { 
+    queryParams.push(`${options.minimum_price_per_night*100}`);
+    if(queryParams.length > 0){
+    queryString += `AND cost_per_night >= $${queryParams.length} `;
+    } else{
+    queryString += `WHERE cost_per_night >= $${queryParams.length} `;
+    }
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night*100}`);
+    if(queryParams.length > 0){
+    queryString += `AND cost_per_night <= $${queryParams.length} `;
+    }else{
+      queryString += `WHERE cost_per_night <= $${queryParams.length} `;
+    }
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    if(queryParams.length > 0){
+    queryString += `AND rating >= $${queryParams.length} `;
+  }else{
+    queryString += `WHERE rating >= $${queryParams.length} `;
+  }
+  }
+
+
+  console.log(queryParams)
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 };
+
 exports.getAllProperties = getAllProperties;
 
 
